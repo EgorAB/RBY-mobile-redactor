e1890987364dbf8e522ed0a2dc1b5474fe21e49b
EgorAB <enterskylife@gmail.com>
2019-05-11 02:32:04 +0300

    :tada:

--- app/src/main/java/com/example/rby_mobile_redactor/MainActivity.kt
+++ app/src/main/java/com/example/rby_mobile_redactor/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.rby_mobile_redactor
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
+import android.support.v7.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,9 +14,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Вставить действие", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        fab.setOnClickListener { view -> run {
+            AlertDialog.Builder(this).run {
+                setTitle("Добавление ячейки")
+                setPositiveButton("Текст") { _, _ -> {}}
+                setNegativeButton("Картинка") { _, _ -> {}}
+            }.create().show()
+                Snackbar.make(view, "Элемент добавлен (нет)", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+            }
         }
     }
 }
