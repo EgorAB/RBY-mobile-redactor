71ddd53446f6ce6cf679276b1653183ab9ba3857
usualhuman <pomayx@gmail.com>
2019-05-11 17:52:01 +0300

    :pig:

--- app/src/main/java/com/example/rby_mobile_redactor/ArticleChooseActivity.kt
+++ app/src/main/java/com/example/rby_mobile_redactor/ArticleChooseActivity.kt
@@ -2,13 +2,13 @@ package com.example.rby_mobile_redactor
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.widget.LinearLayout
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_article_choose.*
-import org.json.JSONArray
+import kotlinx.android.synthetic.main.log_in.*
+import org.json.JSONObject
 import java.io.File
 
 class ArticleChooseActivity : AppCompatActivity() {
@@ -17,24 +17,34 @@ class ArticleChooseActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article_choose)
 
-        supportActionBar!!.displayOptions =
-            ActionBar.DISPLAY_SHOW_HOME or ActionBar.DISPLAY_SHOW_TITLE or ActionBar.DISPLAY_HOME_AS_UP or ActionBar.DISPLAY_USE_LOGO
-        actionBar!!.setIcon(R.drawable.ic_r)
-
-        titleLayout.setOnClickListener { createArticle() }
+        val logInLayout = LayoutInflater.from(this).inflate(R.layout.log_in, null)
+        titleLayout.addView(logInLayout)
+        logIn.setOnClickListener {
+            if (loginField.text.toString() != "" && password.text.toString() != "") {
+            }
+        }
 
         fillTitles()
+
+        newArticle.setOnClickListener { createArticle() }
     }
 
+    private var login = false
+
     private val articlesPath = "articles.json"
+
+    private fun logIn(login: String, password: String) {
+
+    }
 
     private fun fillTitles() {
         if (File(articlesPath).exists()) {
             openFileInput(articlesPath).use { input ->
-                val titles = JSONArray(String(input.readBytes()))
+                val titles = JSONObject(String(input.readBytes()))
                 for (i in 0 until titles.length()) {
-                    val articlePreview = LayoutInflater.from(this).inflate(R.layout.article_preview, null) as LinearLayout
-                    articlePreview.findViewById<TextView>(R.id.title).text = titles.getString(i)
+                    val articlePreview =
+                        LayoutInflater.from(this).inflate(R.layout.article_preview, null) as LinearLayout
+                    articlePreview.findViewById<TextView>(R.id.title).text = titles.getString("title")
                     titleLayout.addView(articlePreview)
                 }
             }
