2f3fb85542dff705d961d9d0cef9d2a7ba8fb153
usualhuman <pomayx@gmail.com>
2019-05-11 12:17:53 +0300

    :pig:

---
+++ app/src/main/java/com/example/rby_mobile_redactor/ArticleChooseActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rby_mobile_redactor
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ArticleChooseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article_choose)
+    }
+}
