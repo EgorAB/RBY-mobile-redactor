90ed28f9cbb003335d592d54fb9f3884995a0b56
usualhuman <pomayx@gmail.com>
2019-05-12 12:07:35 +0300

    :pig:

--- app/src/main/java/com/example/rby_mobile_redactor/ArticleChooseActivity.kt
+++ app/src/main/java/com/example/rby_mobile_redactor/ArticleChooseActivity.kt
@@ -5,11 +5,9 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.LayoutInflater
-import android.widget.Button
-import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
 import com.android.volley.Request
+import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_article_choose.*
@@ -22,6 +20,8 @@ class ArticleChooseActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article_choose)
 
+        File(articlesPath).mkdir()
+
         val logInLayout = LayoutInflater.from(this).inflate(R.layout.log_in, null)
         titleLayout.addView(logInLayout)
         logInLayout.findViewById<Button>(R.id.logIn).setOnClickListener {
@@ -29,33 +29,71 @@ class ArticleChooseActivity : AppCompatActivity() {
             val password = logInLayout.findViewById<EditText>(R.id.password).text.toString()
             if (login != "" && password != "") {
                 logIn(login, password)
+            } else {
+                Toast.makeText(this, "Введите логин и пароль", Toast.LENGTH_LONG).show()
             }
         }
 
         fillTitles()
 
-        newArticle.setOnClickListener { createArticle() }
+        newArticle.setOnClickListener {
+            createArticle()
+        }
     }
 
     private var login = ""
 
-    private val articlesPath = "articles.json"
+    private val articlesPath = "/storage/Android/data/com.example.rby_mobile_redactor/saves"
+    private val address = "http://172.20.20.158:8008"
 
     private fun logIn(login: String, password: String) {
         val queue = Volley.newRequestQueue(this)
         queue.add(JsonObjectRequest(
             Request.Method.POST,
-            "172.20.20.158:80",
-            JSONObject(mapOf(
-                "purpose" to "create_user",
-                "content" to JSONObject(mapOf(
-                    "login" to login,
-                    "password" to password
-                ))
-            )),
+            address,
+            JSONObject(
+                mapOf(
+                    "purpose" to "create_user",
+                    "content" to JSONObject(
+                        mapOf(
+                            "login" to login,
+                            "password" to password
+                        )
+                    )
+                )
+            ),
             { response ->
                 Log.d("RESPONSE", response.toString())
-                //if (response.getString("u_r_created") == "ok")
+                if (response.getString("u_r_created") == "ok") {
+                    uploadAll(queue)
+                    updateTitles()
+                    titleLayout.removeAllViews()
+                    fillTitles()
+                }
+            },
+            { error ->
+                Log.d("ERROR", error.toString())
+            }
+        ))
+    }
+
+    private fun updateTitles() {
+        val queue = Volley.newRequestQueue(this)
+        queue.add(JsonObjectRequest(
+            Request.Method.POST,
+            "http://172.20.20.158:8008",
+            JSONObject(
+                mapOf(
+                    "purpose" to "get_data",
+                    "content" to JSONObject(
+                        mapOf(
+                            "login" to login
+                        )
+                    )
+                )
+            ),
+            { response ->
+                Log.d("RESPONSE", response.toString())
             },
             { error ->
                 Log.d("ERROR", error.toString())
@@ -64,20 +102,48 @@ class ArticleChooseActivity : AppCompatActivity() {
     }
 
     private fun fillTitles() {
-        if (File(articlesPath).exists()) {
+        for (file in File(articlesPath).listFiles()) {
             openFileInput(articlesPath).use { input ->
-                val titles = JSONObject(String(input.readBytes()))
-                for (i in 0 until titles.length()) {
-                    val articlePreview =
-                        LayoutInflater.from(this).inflate(R.layout.article_preview, null) as LinearLayout
-                    articlePreview.findViewById<TextView>(R.id.title).text = titles.getString("title")
-                    titleLayout.addView(articlePreview)
-                }
+                val article = JSONObject(String(input.readBytes()))
+                val articlePreview =
+                    LayoutInflater.from(this).inflate(R.layout.article_preview, null) as LinearLayout
+                articlePreview.findViewById<TextView>(R.id.title).text = article.getString("title")
+                titleLayout.addView(articlePreview)
             }
         }
     }
 
     private fun createArticle() {
-        startActivity(Intent(this, MainActivity::class.java))
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("login", login)
+        intent.putExtra("path", "$articlesPath/${File(articlesPath).listFiles().size}")
+        startActivity(intent)
+    }
+
+    private fun uploadAll(queue: RequestQueue) {
+        assert(login != "")
+        val files = File(articlesPath).listFiles()
+        queue.add(JsonObjectRequest(
+            Request.Method.POST,
+            address,
+            JSONObject(
+                mapOf(
+                    "purpose" to "update_articles",
+                    "content" to JSONObject(
+                        mapOf(
+                            login to JSONObject(files.map { file ->
+                                file.name to file.inputStream().use { input -> input.read() }
+                            }.toMap())
+                        )
+                    )
+                )
+            ),
+            { response ->
+                Log.d("RESPONSE", response.toString())
+            },
+            { error ->
+                Log.d("ERROR", error.toString())
+            }
+        ))
     }
 }
